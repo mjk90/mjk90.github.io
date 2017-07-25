cff3f13cb655d97b894350ac812475bf91e90287
Matt Kelly <mkelly2@live.co.uk>
2017-07-25 11:46:54 +0100

    Working on contact form validation

--- js/contact_me.js
+++ js/contact_me.js
@@ -6,22 +6,6 @@ $(function() {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            event.preventDefault();
-            
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-
-            $('#contactForm').submit();
         },
         filter: function() {
             return $(this).is(":visible");
